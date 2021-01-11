b8bc64896535cddc61bc7c8599d55e55b331876c
Mohamed Farhat <mohamedfarhat01@gmail.com>
2021-01-11 23:24:36 +0400

    Add project files.

---
+++ AutofacWebMvcFive/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System.Web.Mvc;
+using AutofacWebMvcFive.Filters;
+using AutofacWebMvcFive.Repositories;
+
+namespace AutofacWebMvcFive.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IDataRepository _dataRepository;
+
+        public HomeController(IUserRepository userRepository, IDataRepository dataRepository)
+        {
+            _userRepository = userRepository;
+            _dataRepository = dataRepository;
+        }
+
+        [LogActionFilter]
+        public ActionResult Index()
+        {
+            var data = _dataRepository.GetData();
+            var users = _userRepository.GetUsers();
+            return View();
+        }
+
+        public ActionResult AngularJs()
+        {
+            return View();
+        }
+
+        public ActionResult CustomIocJs()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
